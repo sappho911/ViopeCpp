11bed4c5580c591a77e5650eda2a29b06c80b178
sappho911 <blademaster852@hotmail.com>
2023-05-30 13:21:19 +0300

    Printing on screen and Reading input from keyboard and printing it

---
+++ Exercise_02/ex_02_2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int first;
+    int second;
+
+    cout << "Type the first number:";
+    cin >> first;
+
+    cout << "Type the second number:";
+    cin >> second;
+
+    cout << "Sum of the numbers is " << first + second << endl;
+}
