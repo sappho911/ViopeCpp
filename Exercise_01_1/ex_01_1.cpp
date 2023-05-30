1d5f3b0fb5a846f4050648ea76aad3cb172dc414
sappho911 <blademaster852@hotmail.com>
2023-05-30 13:17:06 +0300

    First try at programming with C++ Example solution

--- Exercise_01_1/ex_01_1.cpp
+++ Exercise_01_1/ex_01_1.cpp
@@ -1,1 +1,11 @@
-111
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int number;
+
+    cout << "Input an integer:";
+    cin >> number;
+    cout << "You typed: " << number << endl;
+}
